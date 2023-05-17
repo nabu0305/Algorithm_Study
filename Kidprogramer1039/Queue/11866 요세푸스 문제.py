a467c517c17498ab97c875a2452f4c87bf30a329
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2023-05-17 15:37:58 +0900

    [BOJ] 요세푸스 문제  / 실버5 / 40분(풀이 실패)

--- Kidprogramer1039/Queue/11866 요세푸스 문제.py
+++ Kidprogramer1039/Queue/11866 요세푸스 문제.py
@@ -8,10 +8,10 @@ back = n-1
 
 while back - front != -1:
     for i in range(k-1):
-        nums.append(nums[front])
+        nums.append(nums[front]) #앞의 숫자 2개를 뒤로 옮기고, 큐의 head와 tail 부분을 2칸씩 이동 
         front += 1
         back += 1
-    result.append(nums[front])
+    result.append(nums[front]) # 3칸마다 head 부분을 개별 기록
     front += 1
 
 list_str = str(result)[1:-1] #배열을 문자화 한 후, 양끝을 슬라이스 처리
