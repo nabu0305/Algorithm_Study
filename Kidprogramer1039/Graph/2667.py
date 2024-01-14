2918feb988083a6f9f030c9930ee302dc7703875
Kidprogramer1039 <98392401+Kidprogramer1039@users.noreply.github.com>
2024-01-14 23:58:51 +0900

    [BOJ] 단지번호붙이기 / 실버1 / 풀이실패

--- Kidprogramer1039/Graph/2667.py
+++ Kidprogramer1039/Graph/2667.py
@@ -47,3 +47,76 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 틀린 코드
+# 22:30 ~ 
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def cpxFinder(x,y): # 마을을 발견하고, 발견하면 값을 매기는 재귀 함수
+    global villageNum, tempVil, a
+    if x >= a or y >= a or x < 0 or y < 0:
+        return
+    else:
+        if cpx[x][y] != 1:
+            cpxFinder(x+1, y)
+            cpxFinder(x, y+1)
+        elif cpx[x][y] == 1:
+            villageColoring(x,y)
+            villageDetail.append((villageNum-1, tempVil))
+            tempVil = 0
+            villageNum += 1
+    
+
+
+def villageColoring(x,y): # 마을을 색칠하는 함수
+    global a
+    if x >= a or y >= a or x < 0 or y < 0:
+        return
+    else:    
+        villageCount(x-1,y)
+        villageCount(x+1,y)
+        villageCount(x,y-1)
+        villageCount(x,y+1)
+
+def villageCount(x,y): # 색칠하는 세부 함수
+    global villageNum, tempVil, a
+    if x >= a or y >= a or x < 0 or y < 0:
+        return
+    else:
+        if cpx[x][y] == 1:
+            cpx[x][y] = villageNum
+            tempVil += 1
+            villageColoring(x,y)
+        else:
+            pass
+    
+
+
+def complexCount(a):
+    cpxFinder(0, 0)
+
+
+
+a = int(input())
+villageDetail = [] #마을별 갯수 선행 저장
+villageNum = 2 #마을 개수 세는 함수
+tempVil = 0
+result = []
+cpx = []
+for i in range(a):
+    b = list(map(int, input().strip()))  # 각 줄을 정수 리스트로 변환
+    cpx.append(b)
+    
+complexCount(a-1)
+print(villageNum-2)
+for i in villageDetail:
+    result.append(i[1])
+
+result.sort()
+for i in result:
+    print(i)
